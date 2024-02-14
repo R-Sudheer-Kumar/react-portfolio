590220f5e854119d601d4ef0280b6d4521eb35f9
Rachamadugu Sudheer kumar <rsudheerkumar40@gmail.com>
2024-02-14 20:17:05 +0530

    commit

--- src/firebase.utils.js
+++ src/firebase.utils.js
@@ -1,23 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {getStorage} from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAook0bJxS2SICak3FUwylxOTgpTZhntMo",
-  authDomain: "rsk-portfolio-ab5bd.firebaseapp.com",
-  projectId: "rsk-portfolio-ab5bd",
-  storageBucket: "rsk-portfolio-ab5bd.appspot.com",
-  messagingSenderId: "78059282102",
-  appId: "1:78059282102:web:88f2b9a19f3c754a5d5017",
-  measurementId: "G-YTMDVRBT9P"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID ,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
-export const sudheerImage = 'https://firebasestorage.googleapis.com/v0/b/rsk-portfolio-ab5bd.appspot.com/o/sudheerPhoto.jpg?alt=media&token=63191f1e-c1bd-499f-bb12-23b9a430f678';
+export const sudheerImage = process.env.REACT_APP_SUDHEER_IMAGE;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
